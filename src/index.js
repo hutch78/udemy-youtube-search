4ff1724509f3e25eca348fbfdd7dac0075e1f38e
Jeremy Hutchcraft <jhutchcraft@findsomewinmore.com>
2017-05-20 16:03:36 -0400

    youtube request working

--- src/index.js
+++ src/index.js
@@ -1,10 +1,21 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import YTSearch from 'youtube-api-search';
 
-// Create new Component. This component should produce
-// some HTML
+import SearchBar from './components/search_bar';
+
+const API_KEY = 'AIzaSyCTB2Nsp6RR7T9VJKIkYb_T1dxRs0I0ax4';
+
+YTSearch({key: API_KEY, term: 'surfboards'}, function(data) {
+    console.log(data);
+});
+
 const App = () => {
-    return <div>Hi!</div>;
+    return (
+        <div>
+            <SearchBar />
+        </div>
+    );
 }
 
 ReactDOM.render(<App />, document.querySelector('.container'));
